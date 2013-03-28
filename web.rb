789f030f98b86bb6ae2231751ebab1917f0d125b
Patric Fornasier <patric.fornasier@gmail.com>
2013-03-28 16:19:36 +0530

    pat: unfreezing

--- web.rb
+++ web.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require 'twitter'
 require 'geokit'
-require 'dalli'
-require 'rack-cache'
 require './lib/tweet'
 require './lib/place'
 require './lib/route'
@@ -13,12 +11,11 @@ Geokit::default_formula = :sphere
 
 average_cycling_speed_mph = 5
 
-configure do
-  $cache = Dalli::Client.new
-  use Rack::Cache, :verbose => true, :metastore => $cache, :entitystore => $cache, :allow_reload => false
-end
-
 configure :production do
+  require 'dalli'
+  require 'rack-cache'  
+  $cache = Dalli::Client.new
+  use Rack::Cache, :verbose => true, :metastore => $cache, :entitystore => $cache, :allow_reload => false  
   set :static_cache_control, [:public, :max_age => 60]  
 end
 
@@ -27,20 +24,16 @@ before do
 end
 
 get '/' do
-  erb :frozen
-end
+  tweets = Tweet.load
+  @last_tweet = tweets.first
+  @route = Route.from(tweets)
+  @last_place = @route.last_place
 
-# get '/' do
-#   tweets = Tweet.load
-#   @last_tweet = tweets.first
-#   @route = Route.from(tweets)
-#   @last_place = @route.last_place
-# 
-#   @hours_since_last_place = ((Time.new() - @last_place.visited_at) / 3600).round
-#   @how_far_might_he_have_gone = @hours_since_last_place * average_cycling_speed_mph
-# 
-#   erb :index
-# end
+  @hours_since_last_place = ((Time.new() - @last_place.visited_at) / 3600).round
+  @how_far_might_he_have_gone = @hours_since_last_place * average_cycling_speed_mph
+
+  erb :index
+end
 
 helpers do 
   include Helpers
