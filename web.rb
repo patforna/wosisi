fb102e6f80507ba2575afcdadc61a839dd9ee9c1
Patric Fornasier <patric.fornasier@gmail.com>
2013-03-28 16:16:04 +0530

    pat: killing apigee

--- web.rb
+++ web.rb
@@ -19,13 +19,6 @@ configure do
 end
 
 configure :production do
-  Twitter.configure do |config|                        
-    config.endpoint = 'http://' + ENV['APIGEE_TWITTER_API_ENDPOINT']     
-    config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-    config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
-    config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
-  end
   set :static_cache_control, [:public, :max_age => 60]  
 end
 
