12b3c85b8cfa908b38ea8c9f00ede073603d53d2
Patric Fornasier <patric.fornasier@gmail.com>
2013-03-28 17:40:40 +0530

    pat: tweaks

--- web.rb
+++ web.rb
@@ -1,15 +1,9 @@
 require 'sinatra'
 require 'twitter'
-require 'geokit'
 require './lib/tweet'
 require './lib/place'
 require './lib/route'
 require './lib/helpers'
-
-Geokit::default_units = :kms
-Geokit::default_formula = :sphere
-
-average_cycling_speed_mph = 5
 
 configure :production do
   require 'dalli'
@@ -24,13 +18,12 @@ before do
 end
 
 get '/' do
+  puts "hello world"
   tweets = Tweet.load
+  puts tweets
   @last_tweet = tweets.first
   @route = Route.from(tweets)
   @last_place = @route.last_place
-
-  # @hours_since_last_place = ((Time.new() - @last_place.visited_at) / 3600).round
-  # @how_far_might_he_have_gone = @hours_since_last_place * average_cycling_speed_mph
 
   erb :index
 end
